294b07eadc3fc51afcf65890c38694eee7849af2
Stanislav Sinyagin <ssinyagin@k-open.com>
2004-12-15 17:05:57 +0000

    plugins updated

---
+++ plugins/overview/overview-torruscfg.pl
@@ -0,0 +1,7 @@
+# Overview Torrus plugin configuration
+
+$Torrus::Renderer::styling{'default'}{'ovStylesheet'} = 'ov.css';
+$Torrus::Renderer::styling{'printer'}{'ovStylesheet'} = 'ov-printer.css';
+$Torrus::Renderer::Chooser::template = 'ov/ov-chooser.html';
+
+1;

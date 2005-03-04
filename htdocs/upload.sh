fba10d62f47c3f5c089cd9ed866e08c8d8a12807
Stanislav Sinyagin <ssinyagin@k-open.com>
2005-03-04 23:08:10 +0000

    web docs updated

--- htdocs/upload.sh
+++ htdocs/upload.sh
@@ -6,7 +6,6 @@ if test x"$*" = x; then
   scp -C manpages/*.html $DEST/manpages
   scp -C devdoc/*.html $DEST/devdoc
   scp -C plugins/*.html $DEST/plugins
-  scp -C rrdman/*.html $DEST/rrdman
   scp logo/*.png $DEST/logo
 else
   scp -C $* $DEST

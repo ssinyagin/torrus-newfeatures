391934e782c7d1f093d1c36b74397ae892401248
Stanislav Sinyagin <ssinyagin@k-open.com>
2005-04-29 15:46:18 +0000

    ONS15302 update

--- src/perllib/Torrus/DevDiscover/AxxessIT.pm
+++ src/perllib/Torrus/DevDiscover/AxxessIT.pm
@@ -26,6 +26,8 @@
 # Tested with:
 #
 # Cisco ONS 15305 software release 1.1.1
+# Cisco ONS 15302
+
 
     
 
@@ -62,6 +64,17 @@ our %oiddef =
      
      'axxEdgeDcnManagementPortMode'    => '1.3.6.1.4.1.7546.1.4.1.2.3.2.1.0',
      'axxEdgeDcnManagementPortIfIndex' => '1.3.6.1.4.1.7546.1.4.1.2.3.2.2.0',
+
+     # AXX155E-MIB (ONS 15302)
+     'axx155EDevices'                 => '1.3.6.1.4.1.7546.1.5.1.1',
+
+     'axx155EEthPortTable'            => '1.3.6.1.4.1.7546.1.5.1.2.6.1.2',
+     'axx155EEthPortIfIndex'          => '1.3.6.1.4.1.7546.1.5.1.2.6.1.2.1.2',
+     'axx155EEthPortName'             => '1.3.6.1.4.1.7546.1.5.1.2.6.1.2.1.3',
+     'axx155EEthPortType'             => '1.3.6.1.4.1.7546.1.5.1.2.6.1.2.1.4',
+
+     'axx155EDcnManagementPortMode'   => '1.3.6.1.4.1.7546.1.5.1.2.2.2.2.0',
+     'axx155EDcnManagementPortIfIndex' => '1.3.6.1.4.1.7546.1.5.1.2.2.2.3.0'
      );
 
 
@@ -70,15 +83,20 @@ sub checkdevtype
     my $dd = shift;
     my $devdetails = shift;
 
-    if( not $dd->oidBaseMatch
-        ( 'axxEdgeTypes',
-          $devdetails->snmpVar( $dd->oiddef('sysObjectID') ) ) )
+    if( index( $devdetails->snmpVar( $dd->oiddef('sysObjectID') ),
+               $dd->oiddef('axxEdgeTypes') ) == 0 )
+    {
+        $devdetails->setCap('axxEdge');
+    }
+    elsif( index( $devdetails->snmpVar( $dd->oiddef('sysObjectID') ),
+                  $dd->oiddef('axx155EDevices') ) == 0 )
+    {
+        $devdetails->setCap('axx155E');
+    }
+    else
     {
         return 0;
     }
-
-    # Leave room for AXX155 devices, maybe someone needs them in the future
-    $devdetails->setCap('axxEdge');
 
     $devdetails->setCap('interfaceIndexingManaged');
 
@@ -222,16 +240,89 @@ sub discover
                 }
             }
         }
-        
-        foreach my $ifIndex ( keys %{$data->{'interfaces'}} )
+    }
+    
+    if( $devdetails->hasCap('axx155E') )
+    {
+        my $ethTable =
+            $session->get_table( -baseoid =>
+                                 $dd->oiddef('axx155EEthPortTable') );
+        $devdetails->storeSnmpVars( $ethTable );
+
+        foreach my $port
+            ( $devdetails->
+              getSnmpIndices($dd->oiddef('axx155EEthPortIfIndex')) )
         {
-            if( not defined( $data->{'interfaces'}{$ifIndex}->
-                             {'param'}{'interface-index'} ) )
+            my $ifIndex =
+                $devdetails->snmpVar
+                ($dd->oiddef('axx155EEthPortIfIndex') .'.'. $port);
+
+            my $interface = $data->{'interfaces'}{$ifIndex};
+            next if not defined( $interface );
+
+            my $portName =
+                $devdetails->snmpVar
+                ($dd->oiddef('axx155EEthPortName') .'.'. $port);
+            
+            my $portType =
+                $devdetails->snmpVar
+                ($dd->oiddef('axx155EEthPortType') .'.'. $port);
+
+            $interface->{'param'}{'interface-index'} = $ifIndex;
+            
+            my $type = $portType == 1 ? 'Eth':'Wan';
+            
+            $interface->{'axxInterfaceNick'} =
+                sprintf( '%s_%d', $type, $port );
+            
+            $interface->{'axxInterfaceHumanName'} =
+                sprintf( '%s %d', $type, $port );
+            
+            $interface->{'axxInterfaceComment'} = '';
+            if( length( $portName ) > 0 )
             {
-                delete $data->{'interfaces'}{$ifIndex};
+                $interface->{'axxInterfaceComment'} = $portName;
+            }
+        }
+        
+        # Management interface
+        {
+            my $result = $dd->retrieveSnmpOIDs
+                ( 'axx155EDcnManagementPortMode',
+                  'axx155EDcnManagementPortIfIndex');
+
+            if( defined( $result ) )
+            {
+                if( $result->{'axx155EDcnManagementPortMode'} != 2 )
+                {
+                    Warning('Non-IP mode of Management port is not supported');
+                }
+                else
+                {
+                    my $ifIndex = $result->{'axx155EDcnManagementPortIfIndex'};
+                    
+                    my $interface = $data->{'interfaces'}{$ifIndex};
+                    
+                    $interface->{'param'}{'interface-index'} = $ifIndex;
+                    
+                    $interface->{'axxInterfaceNick'} = 'Management';
+                    
+                    $interface->{'axxInterfaceHumanName'} = 'Management';
+                    
+                    $interface->{'axxInterfaceComment'} = 'Management port';
+                }
             }
         }
     }
+    
+    foreach my $ifIndex ( keys %{$data->{'interfaces'}} )
+    {
+        if( not defined( $data->{'interfaces'}{$ifIndex}->
+                         {'param'}{'interface-index'} ) )
+        {
+            delete $data->{'interfaces'}{$ifIndex};
+        }
+    }    
     
     return 1;
 }

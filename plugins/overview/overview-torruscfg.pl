d1f4d6a7cac5998f50175e8f21f905dba28dd3b5
Christian Schnidrig <christian.schnidrig@gmail.com>
2006-07-27 16:48:24 +0000

    renamed templates

--- plugins/overview/overview-torruscfg.pl
+++ plugins/overview/overview-torruscfg.pl
@@ -2,6 +2,6 @@
 
 $Torrus::Renderer::styling{'default'}{'ovStylesheet'} = 'ov.css';
 $Torrus::Renderer::styling{'printer'}{'ovStylesheet'} = 'ov-printer.css';
-$Torrus::Renderer::Chooser::template = 'ov/ov-chooser.html';
+$Torrus::Renderer::Chooser::template = 'ov/default-chooser.html';
 
 1;
